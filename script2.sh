b1f07db161e8507f8ecbefcf1414ab8dcfa37d15
Iker <ilopezba@esliceu.net>
2026-06-21 19:47:36 +0200

    script1.sh

---
+++ script2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# variables són "paraules" que contenen un valor
+NOM="Antonio"
+
+# read serveix per llegir informació del teclat
+read -p "introdueix un nom: " NOM
+
+echo "El teu nom és ${NOM}"
